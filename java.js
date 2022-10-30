16f3c1f3601162f63bdb973ee14d6dfb6a82f09c
Lukaparulua <100473997+Lukaparulua@users.noreply.github.com>
2022-10-30 15:49:50 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+const name = prompt('გსურთ მთავარ გვერდზე დაბრუნება?');
+
+// hey.addEventListener("click", function(){
+//   // let input=document.createElement("input");
+//   // input.innerText="Education";
+//   // form_id.appendChild(input)
+//   // input.style.backgroundColor="lightBlue"
+//   // input.style.borderRadius="5px"
+//   input.style.display="block"
+//   input.style.marginBottom="5px"
+// })
